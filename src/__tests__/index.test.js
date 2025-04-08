a68a649cb429041bed9ee73672befc8081df5d16
EZTest AI <eztest.ai@commit.com>
2025-04-08 22:16:30 +0530

    Added src/__tests__/index.test.js file

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,8 @@
+const { functionName } = require('../index');
+
+describe('functionName', () => {
+    test('should return expected result', () => {
+        const result = functionName(args);
+        expect(result).toBe(expectedResult);
+    });
+});
